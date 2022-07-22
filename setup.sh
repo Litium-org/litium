214b2d7a53a4baaf51ad8e2d6ef0a4589ee10ee5
dyp1xy <derick.kunz@gmail.com>
2022-07-22 20:40:17 -0300

    gonna work on this later

--- setup.sh
+++ setup.sh
@@ -1,45 +1,41 @@
 #!/bin/bash
+## @author : Dyp1xy
+#
 ## Satisfy dependencies
 ## Update on run
 ## Tests about standalone executables (incomplete)
 
 # constants
 readonly WORKING_DIRECTORY=$(pwd)
-
+readonly TRY_GIT_PULL=false
+readonly GREEN='\033[0;32m' # Green output color
+readonly NC='\033[0m' # No Color
 
 # init/update submodules
-git submodule init update
-
+git submodule update --init
 # lua-https module (love)
-cmake -Bbuild -S. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD\install
-cmake --build build --target install
+lua-https() {
+        cd build/lua-https/
+        cmake -Bbuild -S. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD\install
+        cmake --build build --target install
+
+        echo "done" > done.txt
+        echo -e "${GREEN}[$(date +"%H:%M")]:${NC} Module [ lua-https ] installed "
+
+        cd $WORKING_DIRECTORY
+}
+if [ -s build/lua-https/done.txt ]
+then
+        echo -e "${GREEN}[$(date +"%H:%M")]:${NC} Module lua-htpps already installed"
+else
+        lua-https
+fi
 
 update(){
-        CURRENT_VERS=$(cat ".litversion")
-        GITHUB_VERS=$(curl "https://raw.githubusercontent.com/Litium-org/litium/master/.litversion")
-        if [ $CURRENT_VERS<$GITHUB_VERS ]
-        then
-                echo "Version mismatch"
-                echo "Current version: $CURRENT_VERS"
-                echo "Most recent version: $GITHUB_VERS"
+        if $TRY_GIT_PULL ; then
                 git pull
+        else
+                echo -e "${GREEN}[$(date +"%H:%M")]:${NC} Not checking for updates"
         fi
 }
-srlua(){
-        cd srlua-102
-        make
-        sudo make install
-        cd ..
-}
-compile(){
-
-        return true
-}
 update
-echo "Compiling..."
-if [ !compile ]
-then
-        echo "Build unsuccessful"
-else
-        echo "Build finished successfully"
-fi
